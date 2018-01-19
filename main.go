57523c883821bcdf314abe946207166689e7c85e
Galih Muhammad <galih0muhammad@gmail.com>
2018-01-20 06:41:56 +0700

    Initial step to switch from apex to plain lambda function

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-    "encoding/json"
-
-    "github.com/apex/go-apex"
+    "errors"
+    "log"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
-    apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-        Run()
+var (
+    // ErrNameNotProvided is thrown when a name is not provided
+    ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
+)
 
-        return nil, nil
-    })  
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+    // stdout and stderr are sent to AWS CloudWatch Logs
+    log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+
+    // If no name is provided in the HTTP request body, throw an error
+    if len(request.Body) < 1 {
+        return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+    }
+
+    return events.APIGatewayProxyResponse{
+        Body:       "Hello " + request.Body,
+        StatusCode: 200,
+    }, nil
+
+}
+
+
+func main() {
+    lambda.Start(Handler)
 }
