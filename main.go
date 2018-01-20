d073dbfc2715ad0b3221517b995f4cebace81fa8
Galih Muhammad <galih0muhammad@gmail.com>
2018-01-20 17:57:34 +0700

    Get the first version of vanilla golang lambda function

--- main.go
+++ main.go
@@ -1,33 +1,25 @@
 package main
 
 import (
-    "errors"
     "log"
     "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-    // ErrNameNotProvided is thrown when a name is not provided
-    ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-)
-
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-    // stdout and stderr are sent to AWS CloudWatch Logs
     log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
 
-    // If no name is provided in the HTTP request body, throw an error
-    if len(request.Body) < 1 {
-        return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+    if len(request.Body) >= 1 {
+        log.Printf("Body request received: %s", request.Body)
     }
+
+    Run()
 
     return events.APIGatewayProxyResponse{
         Body:       "Hello " + request.Body,
         StatusCode: 200,
     }, nil
-
 }
 
 
